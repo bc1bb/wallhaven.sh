d1482b435e0691bf606740549e0f41dca493b00a
Jus de Patate <jusdepatate@protonmail.com>
2021-12-16 22:02:33 +0100

    2022 edition
    supports XDG picture folder variable, correct a few bugs and xdg-open after every download

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # `wallhaven.sh`
 This is a tiny script that downloads a random image from Wallhaven and then (if user asked for) puts it as MATE wallpaper.
 
+(It will always download to `$XDG_PICTURES_DIR/wallhaven/` unless not set, it will download to `$HOME/Images/wallhaven/`)
 ## Requirements
-- MATE Desktop if you want to put wallpaper,
+- MATE Desktop **if you want to put wallpaper**,
 - `curl`,
 - `cut`,
 - `grep`,
 - `bash`. (I haven't tested compatibility with any other shell)
 (It doesn't require any API Key)
 
-## Installation
-`curl https://raw.githubusercontent.com/jusdepatate/wallhaven.sh/master/wallhaven.sh > wallhaven.sh`
+```
+curl https://raw.githubusercontent.com/jusdepatate/wallhaven.sh/master/wallhaven.sh > wallhaven.sh
+chmod +x wallhaven.sh
+./wallhaven.sh
+```
--- wallhaven.sh
+++ wallhaven.sh
@@ -1,41 +1,48 @@
-#!/bin/bash
-# Jus de Patate - 2020 - jusdepatate@protonmail.com (github:jusdepatate)
+#!/usr/bin/env bash
+# Jus de Patate - 2020-2021 - jusdepatate@protonmail.com (github:jusdepatate)
 # Script that changes your wallpaper to a random Wallhaven image
 # Everything started with this command: `curl -s https://wallhaven.cc/random | sed -n 's/.*class="preview" href="https:\/\/wallhaven.cc\/w\/\([^"]*\).*/\1/p' | head -n1 && echo`
-# Explained: curl has argument `s` so we don't have the shitty loading bar, sed tries to output all link with class "preview" and goes to wallhaven.cc/w/, head gives only the first line, `echo` is empty so that we have a new line.
+# Explained: curl has argument `s` so we don't have a loading bar, sed tries to output all link with class "preview" and goes to wallhaven.cc/w/, head gives only the first line, `echo` is empty so that we have a new line.
 
 fatal() {
 	echo -e "$*"
 	exit 1
 }
 
-id=$(curl -s https://wallhaven.cc/random | sed -n 's/.*class="preview" href="https:\/\/wallhaven.cc\/w\/\([^"]*\).*/\1/p' | head -n1 || fatal "Couldn't get random image id")
+id="$(curl -s https://wallhaven.cc/random | sed -n 's/.*class="preview" href="https:\/\/wallhaven.cc\/w\/\([^"]*\).*/\1/p' | head -n1 || fatal "Couldn't get random image id")"
 # gives the id of a random Wallhaven image
-miniid=$(echo $id | cut -c1-2)
+miniid="$(echo "$id" | cut -c1-2)"
 # first 2 letters of the id to build the url
 
 png="https://w.wallhaven.cc/full/$miniid/wallhaven-$id.png"
 jpg="https://w.wallhaven.cc/full/$miniid/wallhaven-$id.jpg"
 # build probable URLs
 
-if curl -Is $png | head -n1 | grep "404" > /dev/null || fatal "Couldn't check if $png gives 404 or not"; then
-	url=$jpg
+if [ "$(curl -s "$png" | head -n1 | grep "404" > /dev/null || fatal "Couldn't check if $png gives 404 or not")" ]; then
+	url="$jpg"
 	type="jpg"
-elif curl -Is $jpg | head -n1 | grep "404" > /dev/null || fatal "Couldn't check if $jpg gives 404 or not"; then
-	url=$png
+elif [ "$(curl -s "$jpg" | head -n1 | grep "404" > /dev/null || fatal "Couldn't check if $jpg gives 404 or not")" ]; then
+	url="$png"
 	type="png"
 fi
 # test probable URLs, final url will be $url
-file="$HOME/Images/wallhaven/$id.$type"
-folder="$HOME/Images/wallhaven/"
+
+if [ ! "$XDG_PICTURES_DIR" == "" ]; then
+  file="$XDG_PICTURES_DIR/wallhaven/$id.$type"
+  folder="$XDG_PICTURES_DIR/wallhaven/"
+else
+  file="$HOME/Images/wallhaven/$id.$type"
+  folder="$HOME/Images/wallhaven/"
+fi
 
 if [ ! -d "$folder" ]; then
-	mkdir "$folder"
+	mkdir -p "$folder"
 fi
 
 touch "$file" || fatal "Couldn't create file $file"
 curl -s "$url" > "$file" || fatal "Couldn't download $url"
 echo "Image saved in $file"
+xdg-open "$file"
 
 # Use this setting only on MATE as this line will work only on MATE
 if [[ "$1" = "--wallpaper" ]] || [[ "$1" == "-w" ]]; then
